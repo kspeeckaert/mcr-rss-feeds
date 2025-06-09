3c0b74a00ac2b8e4c0c86f95c9cfe0e67f508fd5
Kristof <you@example.com>
2025-06-09 11:08:38 +0200

    Even more code improvements!

--- mcr_to_rss.py
+++ mcr_to_rss.py
@@ -1,15 +1,15 @@
 import argparse
-from datetime import datetime, timezone
 import logging
 from pathlib import Path
 from typing import Any
+from datetime import datetime, timezone
 import requests
 from requests.exceptions import HTTPError
 from feedgen.feed import FeedGenerator
 
 
-BASE_MCR_URL = "https://mcr.microsoft.com/api/v1/catalog"
-FEED_DIR = "feeds"
+BASE_MCR_URL = 'https://mcr.microsoft.com/api/v1/catalog'
+BASE_ARTIF_URL = 'https://mcr.microsoft.com/en-us/artifact/mar'
 
 
 def retrieve_json(url: str) -> Any:
@@ -17,12 +17,12 @@ def retrieve_json(url: str) -> Any:
     try:
         response.raise_for_status()
     except HTTPError as e:
-        logging.error(f'Web service returned {e.response.status_code} - {e.response.content}')
+        logging.error(f'URL returned {e.response.status_code} - {e.response.content}')
         raise
     return response.json()
 
 
-def generate_feed(repo: str) -> None:
+def generate_feed(repo: str, feed_dir: str) -> None:
 
     # Get the repository details for the channel
     logging.info('Retrieving repository details...')
@@ -32,7 +32,7 @@ def generate_feed(repo: str) -> None:
     fg.title(data['name'])
     # Make sure to always have a link, otherwise the RSS file is invalid
     if (link := data.get('projectWebsite')) is None:
-        link = f'https://mcr.microsoft.com/en-us/artifact/mar/{repo}/tags'
+        link = f'{BASE_ARTIF_URL}/{repo}/tags'
     fg.link(href=link, rel='alternate')
     fg.description(data.get('shortDescription'))
     fg.lastBuildDate(datetime.now(timezone.utc))
@@ -49,7 +49,7 @@ def generate_feed(repo: str) -> None:
     for tag in tag_data:
         fe = fg.add_entry()
         fe.title(tag['name'])
-        fe.link(href=f'https://mcr.microsoft.com/en-us/artifact/mar/{repo}/tags')
+        fe.link(href=f'{BASE_ARTIF_URL}/{repo}/tags')
         fe.published(tag.get('createdDate'))
         fe.updated(tag.get('lastModifiedDate'))
         # Description is the command to pull the container image
@@ -57,33 +57,35 @@ def generate_feed(repo: str) -> None:
         fe.guid(f'{repo}:{tag['name']}', permalink=False)
 
 
-    output_file = Path(FEED_DIR,f'{repo.replace("/", "_")}.xml')
+    output_file = Path(feed_dir,f'{repo.replace('/', '_')}.xml')
     logging.info(f'Writing to {output_file}...')
     fg.rss_file(output_file)
-    logging.info(f"RSS feed saved.")
+    logging.info(f'RSS feed saved.')
 
 
-def process_repo_list(filename:str) -> None:
+def process_repo_list(filename:str, feed_dir:str) -> None:
     # Open repositories list file, each line is a separate entry
     with open (filename) as f:
-        repos = f.read().strip().splitlines()
+        repos: List[str] = f.read().strip().splitlines()
     logging.info(f'Found {len(repos)} repositories to process.')
     
     # Ensure folder exists
-    Path(FEED_DIR).mkdir(exist_ok=True)
+    Path(feed_dir).mkdir(exist_ok=True)
 
     for repo in repos:
         try:
             logging.info(f'Generating feed for {repo}...')
-            generate_feed(repo)
+            generate_feed(repo, feed_dir)
         except Exception as e:
             logging.error(f'Failed to generate feed for {repo}: {e!r}')
     logging.info('Finished generating feeds.')
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Generate RSS feeds for MCR repos.")
-    parser.add_argument("filename", help="Filename containing list of repositories")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Enable debug logging")
+    parser = argparse.ArgumentParser(description='Generate RSS feeds for MCR repos.')
+    parser.add_argument('filename', help='Filename containing list of repositories')
+    parser.add_argument('--feed-dir', help='Folder to output the resulting XML files', default='feeds')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable debug logging')
     
     args = parser.parse_args()
 
@@ -91,12 +93,12 @@ if __name__ == '__main__':
     log_level = logging.DEBUG if args.verbose else logging.INFO
     logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
     # Avoid logging from requests
-    logging.getLogger("requests").setLevel(logging.WARNING)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger('requests').setLevel(logging.WARNING)
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
 
     # Verify if file exists
     if not Path(args.filename).is_file():
-        logging.error(f"File not found: {args.filename}")
+        logging.error(f'File not found: {args.filename}')
         exit(1)
-
-    process_repo_list(args.filename)
+    print(args)
+    process_repo_list(args.filename, args.feed_dir)
